d5b78aebb08e5a15a5262ee08e1c59f5417dc2ca
Vanessa Su <vnessa.su@gmail.com>
2021-04-14 16:30:24 -0400

    Add sage constructor test
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

---
+++ __tests__/sages.test.js
@@ -0,0 +1,14 @@
+import Sage from './../src/js/sages.js';
+
+describe('Sage constructor and methods', () => {
+  let reusableSage;
+  
+  beforeEach(() => {
+    reusableSage = new Sage("skill", 10);
+  });
+
+  test('should create a sage object with two attributes', () => {
+    expect(reusableSage.type).toEqual("skill");
+    expect(reusableSage.amount).toEqual(10);
+  });
+});
