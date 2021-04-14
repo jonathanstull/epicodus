de980160f03a9e84cb67e18e4787ff62e1d8dbb9
Vanessa Su <vnessa.su@gmail.com>
2021-04-14 19:16:10 -0400

    Add addRoom method test case
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>
    Co-authored-by: Carlos Mendez <carlosmendez86@gmail.com>

--- __tests__/levels.test.js
+++ __tests__/levels.test.js
@@ -12,9 +12,17 @@ describe('Level', () => {
     reusableLevel = new Level();
   });
 
-  it('should create a new level with a battle room, an office, and the wilderness', () => {
+  it('should create a new level with a battle room, an office, and Discord channel March Questions', () => {
     expect(reusableLevel.battle).toEqual({});
     expect(reusableLevel.office).toEqual({});
-    expect(reusableLevel.wilderness).toEqual({});
+    expect(reusableLevel.marchQuestions).toEqual({});
   });
+
+  it('should add room object to appropriate attribute according to type', () => {
+    let room = {type: "office"};
+    reusableLevel.addRoom(room);
+    expect(reusableLevel.battle).toEqual({});
+    expect(reusableLevel.office).toEqual({{type: "office"}});
+    expect(reusableLevel.marchQuestions).toEqual({});
+  })
 });
---
+++ src/js/levels.js
@@ -0,0 +1,8 @@
+export default class Level {
+
+  constructor() {
+    this.battle = {};
+    this.office = {};
+    this.marchQuestions = {};
+  }
+}
