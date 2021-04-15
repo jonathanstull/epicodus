a09cfb54ca0cb43cb58826e85e10421e5b30c85b
yesthecarlos <carlosmendez86@gmail.com>
2021-04-15 14:37:06 -0700

    pass move sage test
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

--- src/js/levels.js
+++ src/js/levels.js
@@ -21,7 +21,24 @@ export default class Level {
   }
 
   changeSageRoom(currentRoomId, nextRoomId){
-    
+    let roomObject;
+    if(currentRoomId in this.battle) {
+      roomObject = this.battle[currentRoomId];
+    } else if(currentRoomId in this.office) {
+      roomObject = this.office[currentRoomId];
+    } else if(currentRoomId in this.marchQuestions) {
+      roomObject = this.marchQuestions[currentRoomId];
+    }
+    let sageObject = roomObject.sages.get(1); //needs fixing later for if multiple sages are in one room
+    roomObject.sages.delete(1);
+
+    if(nextRoomId in this.battle) {
+      this.battle[nextRoomId].sages.set(1, sageObject);
+    } else if(nextRoomId in this.office) {
+      this.office[nextRoomId].sages.set(1, sageObject);
+    } else if(nextRoomId in this.marchQuestions) {
+      this.marchQuestions[nextRoomId].sages.set(1, sageObject);
+    }
   }
 }
 
